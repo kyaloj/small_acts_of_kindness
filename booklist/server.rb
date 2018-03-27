f936f4e9517c012d48f239db526a3d3c72c4616e
Justus <mj.kyalo@gmail.com>
2018-03-27 16:25:22 +0300

    WIP

--- booklist/server.rb
+++ booklist/server.rb
@@ -55,13 +55,26 @@ namespace '/api/v1' do
     content_type 'application/json'
   end
 
+  helpers do
+    def base_url
+      @base_url ||= "#{request.env['rack.url_scheme']}://{request.env['HTTP_HOST']}"
+    end
+
+    def json_params
+      begin
+        JSON.parse(request.body.read)
+      rescue
+        halt 400, { message:'Invalid JSON' }.to_json
+      end
+    end
+  end
+
   get '/books' do
     books = Book.all
 
     [:title, :isbn, :author].each do |filter|
       books = books.send(filter, params[filter]) if params[filter]
     end
-
     books.map { |book| BookSerializer.new(book) }.to_json
   end
 
@@ -70,5 +83,15 @@ namespace '/api/v1' do
     halt(404, { message:'Book Not Found'}.to_json) unless book
     BookSerializer.new(book).to_json
   end
-  
+
+  post '/books ' do
+    book = Book.new(json_params)
+    if book.save
+      response.headers['Location'] = "#{base_url}/api/v1/books/#{book.id}"
+      status 201
+    else
+      status 422
+      body BookSerializer.new(book).to_json
+    end
+  end  
 end
