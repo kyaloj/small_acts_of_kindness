2f1b43827858293b8949e47742e32f6140613c5a
Justus <mj.kyalo@gmail.com>
2018-04-01 14:35:07 +0300

    jumpstart sinatra init commit

---
+++ songs_by_sinatra/main.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+
+get '/' do
+    erb :home
+end
+
+__END__
+@@home
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Songs By Sinatra</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <header>
+            <h1>Songs By Sinatra</h1>
+            <nav>
+                <ul>
+                    <li><a href="/" title="Home">Home</a></li>
+                    <li><a href="/about" title="About">About</a></li>
+                    <li><a href="/contact" title="Contact">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+        <section>
+            <p>
+                Welcome to this website all about the songs of the great
+                Frank Sinatra
+            </p>
+        </section>
+    </body>
+</html>
