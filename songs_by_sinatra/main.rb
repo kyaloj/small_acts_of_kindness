ad47490645e02ab73dc57fb190ac48d2d23388b9
Justus <mj.kyalo@gmail.com>
2018-04-02 13:29:36 +0300

    404

--- songs_by_sinatra/main.rb
+++ songs_by_sinatra/main.rb
@@ -2,13 +2,17 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/' do
-    erb :home
+  erb :home
 end
 
 get '/about' do
-    erb :about
+  erb :about
 end
 
 get '/contact' do
-    erb :contact
+  erb :contact
+end
+
+not_found do
+  erb :not_found
 end
