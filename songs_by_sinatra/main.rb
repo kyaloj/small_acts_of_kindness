105abbbe6d82cd7d65461dfcee488b842e3f5b07
Justus <mj.kyalo@gmail.com>
2018-04-05 07:58:17 +0300

    sessions

--- songs_by_sinatra/main.rb
+++ songs_by_sinatra/main.rb
@@ -4,6 +4,13 @@ require 'slim'
 require 'sass'
 require './song'
 
+configure do
+  set :session_secret, 'arsndsnfsjdnjnvnvdjfnvjfjsdndsjvndvjnjnjnjdjj'
+  enable :sessions
+  set :username, 'frank'
+  set :password, 'sinatra'
+end
+
 get('/styles.css'){ scss :styles }
 
 get '/' do
@@ -21,4 +28,30 @@ end
 
 not_found do
   slim :not_found
+end
+
+get '/set/:name' do
+  session[:name] = params[:name]
+end
+
+get '/get/hello' do
+  "Hello #{session[:name]}"
+end
+
+get '/login' do
+  slim :login
+end
+
+post '/login' do
+  if params[:username] == settings.username && params[:password] == settings.password
+    session[:admin] = true
+    redirect to('/songs')
+  else
+    slim :login
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect to('/login')
 end
