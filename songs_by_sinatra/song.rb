0298ee9525d90f031dd3f197740ad178e604218a
Justus <mj.kyalo@gmail.com>
2018-04-06 08:12:23 +0300

    song helper

--- songs_by_sinatra/song.rb
+++ songs_by_sinatra/song.rb
@@ -2,59 +2,77 @@ require 'dm-core'
 require 'dm-migrations'
 
 configure :development do
-    DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
 end
 
+# Song
 class Song
-    include DataMapper::Resource
-    property :id, Serial
-    property :title, String
-    property :lyrics, Text
-    property :length, Integer
-    property :released_on, Date
+  include DataMapper::Resource
+  property :id, Serial
+  property :title, String
+  property :lyrics, Text
+  property :length, Integer
+  property :released_on, Date
 
-    def released_on=date
-        super Date.strptime(date, '%m/%d/%Y')
-    end
+  def released_on=date
+    super Date.strptime(date, '%m/%d/%Y')
+  end
 end
+
+# Helper
+module SongHelpers
+  def find_songs
+    @songs = Song.all
+  end
+
+  def find_song
+    Song.get(params[:id])
+  end
+
+  def create_song
+    @song = Song.create(params[:song])
+  end
+end
+
+helpers SongHelpers
 
 DataMapper.finalize
 
 get '/songs' do
-    @songs = Song.all
-    slim :songs
+  find_songs
+  slim :songs
 end
 
 get '/songs/new' do
-    halt(401,'Not Authorized') unless session[:admin]
-    @song = Song.new
-    slim :new_song
+  halt(401,'Not Authorized') unless session[:admin]
+  @song = Song.new
+  slim :new_song
 end
 
 get '/songs/:id' do
-    @song = Song.get(params[:id])
-    slim :song
+  @song = find_song
+  slim :song
 end
 
 post '/songs' do
-    song = Song.create(params[:song])
-    redirect to("/songs/#{song.id}")
+  create_song
+  redirect to("/songs/#{song.id}")
 end
 
 get '/songs/:id/edit' do
-    @song = Song.get(params[:id])
-    slim :edit_song
+  @song = find_song
+  slim :edit_song
 end
 
 put '/songs/:id' do
-    song = Song.get(params[:id])
-    song.update(params[:song])
-    redirect to("/songs/#{song.id}")
+  song = find_song
+  song.update(params[:song])
+  redirect to("/songs/#{song.id}")
 end
 
 delete '/songs/:id' do
-    Song.get(params[:id]).destroy
-    redirect to('/songs')
+  find_song.destroy
+  redirect to('/songs')
 end
 
 
